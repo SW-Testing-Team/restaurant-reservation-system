58507c2006bd864ecb0809e379af53ee7318c501
kanzyhany <kanzyhany06@gmail.com>
2025-11-23 21:12:05 +0200

    app.js config module added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,6 +7,8 @@ import { MenuModule } from './menu/menu.module';
 import { FeedbackModule } from './feedback/feedback.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [
@@ -15,6 +17,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     MenuModule,
     FeedbackModule,
     DashboardModule,
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(
       'mongodb+srv://admin:C6xIzFO0lQyCDdCW@main.dzqmb8v.mongodb.net/',
     ),
