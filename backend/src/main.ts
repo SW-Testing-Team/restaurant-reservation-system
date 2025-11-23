508db2aed4b10767e6a8890cec464798b120cdb5
kanzyhany <kanzyhany06@gmail.com>
2025-11-23 23:39:59 +0200

    backend uodate

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  // const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  // await app.listen(process.env.PORT ?? 3000);
+
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
